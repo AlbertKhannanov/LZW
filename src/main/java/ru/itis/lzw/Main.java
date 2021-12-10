93d20b1aa1772207480fc66fd8bd629179ff02d4
Albert <xannanov.albert@mail.ru>
2021-12-11 02:21:39 +0300

    ready

--- src/main/java/ru/itis/lzw/Main.java
+++ src/main/java/ru/itis/lzw/Main.java
@@ -1,16 +1,29 @@
 package ru.itis.lzw;
 
+import javafx.util.Pair;
+import ru.itis.lzw.algorithm.BWT;
 import ru.itis.lzw.algorithm.LZW;
+
+import java.util.Scanner;
+
 
 public class Main {
 
+    private final static BWT bwt = new BWT();
     private final static LZW lzw = new LZW();
     private final static LZW.Prepare lzwPrepare = new LZW.Prepare();
     private final static LZW.Decode lzwDecode = new LZW.Decode();
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+
+
         String source = lzwPrepare.readFile("D:\\Another\\Univercity\\Тесты\\LZW\\src\\main\\test.txt");
+        Pair<String, Integer> bwtResult = bwt.modifiedSequence(source);
+
+        source = bwtResult.getKey();
         lzw.setDictionary(lzwPrepare.initDictionary(source));
 
         String encoded = lzw.algorithm(source);
@@ -20,8 +33,10 @@ public class Main {
 
         System.out.println("\n------------ DECODE ------------");
 
+        System.out.println(bwtResult);
         lzwDecode.initDictionary(source);
         String decode = lzwDecode.decode(encoded);
-        System.out.println(decode);
+
+        System.out.println(bwt.restoreInitString(bwtResult));
     }
 }
---
+++ src/main/java/ru/itis/lzw/algorithm/BWT.java
@@ -0,0 +1,100 @@
+package ru.itis.lzw.algorithm;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BWT {
+
+    public Pair<String, Integer> modifiedSequence(String src) {
+        // выводим все символы под подходящем
+
+        ArrayList<String> cycleOffset = getCycleOffsets(src);
+
+        StringBuilder resultTemp = new StringBuilder();
+        int index = 0;
+        for (String cyc : cycleOffset) {
+            resultTemp.append(cyc.substring(cyc.length() - 1));
+            if (cyc.equals(src)) {
+                index = cycleOffset.indexOf(cyc);
+            }
+        }
+
+        return new Pair<>(resultTemp.toString(), index);
+    }
+
+    public String restoreInitString(Pair<String, Integer> bwtEncode) {
+        ArrayList<StringBuilder> bwtDecode = new ArrayList<>();
+
+        for (int i = 0; i < bwtEncode.getKey().length(); i++) {
+            bwtDecode.add(new StringBuilder(bwtEncode.getKey().substring(i, i + 1)));
+        }
+        quickSort(bwtDecode, 0, bwtDecode.size() - 1);
+
+        for (int i = 0; i < bwtEncode.getKey().length() - 1; i++) {
+            for (int j = 0; j < bwtEncode.getKey().length(); j++) {
+                bwtDecode.get(j).insert(0, bwtEncode.getKey().charAt(j));
+            }
+            quickSort(bwtDecode, 0, bwtDecode.size() - 1);
+        }
+
+        return bwtDecode.get(bwtEncode.getValue()).toString();
+    }
+
+    public void quickSort(ArrayList<StringBuilder> list, int low, int high) {
+        if (list.size() == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        StringBuilder opora = new StringBuilder(list.get(middle));
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (list.get(i).toString().compareTo(opora.toString()) < 0) {
+                i++;
+            }
+
+            while (list.get(j).toString().compareTo(opora.toString()) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                StringBuilder temp = new StringBuilder(list.get(i));
+
+                list.get(i).setLength(0);
+                list.get(i).append(list.get(j));
+
+                list.get(j).setLength(0);
+                list.get(j).append(temp);
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSort(list, low, j);
+
+        if (high > i)
+            quickSort(list, i, high);
+    }
+
+    private ArrayList<String> getCycleOffsets(String str) {
+        // находим циклические сдвиги
+        ArrayList<String> cycleOffset = new ArrayList<>();
+
+        StringBuilder cycleTemp = new StringBuilder(str);
+        for (int i = 0; i < str.length(); i++) {
+            cycleOffset.add(cycleTemp.toString());
+            cycleTemp.append(cycleTemp.substring(1));
+            cycleTemp.append(cycleTemp.substring(0, 1));
+            cycleTemp.delete(0, str.length());
+        }
+        Collections.sort(cycleOffset);
+
+        return cycleOffset;
+    }
+}
